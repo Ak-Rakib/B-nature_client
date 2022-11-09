8c79bb67c3bacec0184a415fb8f61e2f7e1acc80
unknown <ak-rakib>
2022-11-08 22:22:04 -0800

    showing image full screen

--- src/Pages/ServiceDetails/ServiceDetailsCard.js
+++ src/Pages/ServiceDetails/ServiceDetailsCard.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaWindowClose } from 'react-icons/fa';
+import './ServiceDetailsCard.css';
 
 const ServiceDetailsCard = ({ data }) => {
-    const { img, name, description, price } = data
+    const { img, name, description, price, _id } = data
+
+    const [model, setModel] = useState(false);
+    const [fullScreen, setFullScreen] = useState('');
+
+    const showFullScreen = img => {
+        setFullScreen(img);
+        setModel(true);
+    }
+
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-            <img src={img} alt="Album" />
-            <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>${price}</p>
-                <p>{description}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Review</button>
+        <div>
+            <div className={model? "model open" : "model"}>
+                <img src={fullScreen} alt="" />
+               <FaWindowClose onClick={() => setModel(false)}></FaWindowClose>
+            </div>
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <img onClick={() => showFullScreen(img)} src={img} alt="Album" />
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>${price}</p>
+                    <p>{description}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Review</button>
+                    </div>
                 </div>
             </div>
         </div>
