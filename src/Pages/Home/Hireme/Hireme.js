d2eec43e0b8833daad257242d0115409c143b382
unknown <ak-rakib>
2022-11-09 01:52:16 -0800

    hire option update

--- src/Pages/Home/Hireme/Hireme.js
+++ src/Pages/Home/Hireme/Hireme.js
@@ -3,15 +3,11 @@ import image from '../../../images/banner/images.png'
 
 const HireMe = () => {
     return (
-        <div className='mt-16 mb-12'>
-            <div className='mx-auto w-1/3'>
-                <img className='mx-auto w-1/3' src={image} alt="" />
-            </div>
-            <div className='text-center'>
+        <div className="flex flex-col w-full border-opacity-50 mt-24 mb-20">
+            <div className="divider w-1/3 mx-auto mb-10"> <img className=' mx-auto w-1/3' src={image} alt="" /></div>
+            <div className="grid h-20 card rounded-box place-items-center">
                 <h1 className='text-3xl font-bold'>B-nature Studio</h1>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, sequi?</p>
-            </div>
-            <div className='text-center mt-3'>
                 <button className='btn btn-outline'>Hire Me</button>
             </div>
         </div>
